a2b2e5166ce10a2db1f0239f657afcf55ab265b1
dianavfreitas <dianavfreitass@gmail.com>
2024-09-03 11:10:25 -0400

    Pushing updated images folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,12 @@
-import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Pages/HomePage";
-export default function App() {
+import ContactForm from ".\\./pages/Contact";
+// import HomePage from "./pages/Homepage";
+
+function App() {
   return (
-    <div>
-      <Router>
-        <Header />
-        <div>
-          <Routes>
-            {/* <Route path="/" element={<Home />} /> */}
-            {/* <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/career-generator" element={<CareerGenerator />} /> */}
-          </Routes>
-        </div>
-        <Footer />
-      </Router>
+    <div className="App">
+      <ContactForm />
     </div>
   );
 }
+
+export default App;
